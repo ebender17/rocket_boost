09f20d9d07c80a2ab4c7b97ba706f28c46a1226f
Emily Bender <bender.emily1@gmail.com>
2020-09-12 16:56:17 -0400

    Rocket movement, thrust audio, and basic level design

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,32 +5,74 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>(); 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
         
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                print("OK");
+                break;
+            default:
+                //kill player 
+                print("Dead");
+                break;
+        }
+    }
+
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Space pressed."); 
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play(); 
+            }
+        } else
+        {
+            audioSource.Stop(); 
         }
+
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation 
+        float rotationThisFrame = rcsThrust * Time.deltaTime; 
+
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left.");
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right.");
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
+
 }
